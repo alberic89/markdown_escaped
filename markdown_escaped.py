ad0797a241cbe54b202d50079bd51c715892261b
alberic89 <alberic89@gmx.com>
2023-06-24 12:30:42 +0200

    Optimise +++

--- markdown_escaped.py
+++ markdown_escaped.py
@@ -2,49 +2,19 @@ import re
 import xml.etree.ElementTree as etree
 from markdown import util
 from markdown.extensions import Extension
-from markdown.blockprocessors import BlockProcessor
+from markdown.blockprocessors import BlockQuoteProcessor
 
-SUP_RE = r"(\^)([\S]+?)(\^)"
-SUB_RE = r"(\~)([\S]+?)(\~)"
+class BlockQuoteEscaped(BlockQuoteProcessor):
 
-class BlockQuoteEscaped(BlockProcessor):
-
-    RE = re.compile(r'(^|\n)[ ]{0,3}(>|&gt;)[ ]?(.*)')
-
-    def test(self, parent, block):
-        return bool(self.RE.search(block)) and not util.nearing_recursion_limit()
-
-    def run(self, parent, blocks):
-        block = blocks.pop(0)
-        m = self.RE.search(block)
-        if m:
-            before = block[:m.start()]  # Lines before blockquote
-            # Pass lines before blockquote in recursively for parsing first.
-            self.parser.parseBlocks(parent, [before])
-            # Remove ``> `` from beginning of each line.
-            block = '\n'.join(
-                [self.clean(line) for line in block[m.start():].split('\n')]
-            )
-        sibling = self.lastChild(parent)
-        if sibling is not None and sibling.tag == "blockquote":
-            # Previous block was a blockquote so set that as this blocks parent
-            quote = sibling
-        else:
-            # This is a new blockquote. Create a new parent element.
-            quote = etree.SubElement(parent, 'blockquote')
-        # Recursively parse block with blockquote as parent.
-        # change parser state so blockquotes embedded in lists use `p` tags
-        self.parser.state.set('blockquote')
-        self.parser.parseChunk(quote, block)
-        self.parser.state.reset()
+    RE = re.compile(r'(^|\n)[ ]{0,3}&gt;[ ]?(.*)')
 
     def clean(self, line):
-        """ Remove ``>`` from beginning of a line. """
+        """ Remove ``&gt;`` from beginning of a line. """
         m = self.RE.match(line)
-        if line.strip() == ">" or line.strip() == "&gt;":
+        if line.strip() == "&gt;":
             return ""
         elif m:
-            return m.group(3)
+            return m.group(2)
         else:
             return line
 
